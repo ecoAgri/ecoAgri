6874aea234305429b2a471788a59b199825892b1
Pasindu-Lakmal <pasindu1llak@gmail.com>
2022-09-25 08:41:56 +0530

    payment

---
+++ ecoAgriFrontEnd/src/store/orderApiCalls.js
@@ -0,0 +1,96 @@
+import { publicRequest, userRequest } from "../requestMethods";
+import {
+    getOrderStart,
+    getOrderSuccess,
+    getOrderFailure,
+    deleteOrderStart,
+    deleteOrderSuccess,
+    deleteOrderFailure,
+    updateOrderStart,
+    updateOrderSuccess,
+    updateOrderFailure,
+    addOrderStart,
+    addOrderSuccess,
+    addOrderFailure,
+    countOrderStart,
+    countOrderSuccess,
+    countOrderFailure,
+} from "./orderSlice";
+
+export const getOrders = async (dispatch, token) => {
+  dispatch(getOrderStart());
+  try {
+    const res = await userRequest.get("/orders", {
+      headers: {
+        "Content-Type": "application/json",
+        token: `Bearer ${token}`,
+      },
+    });
+    dispatch(getOrderSuccess(res.data));
+  } catch (err) {
+    dispatch(getOrderFailure());
+  }
+};
+
+export const deleteOrder = async (id, dispatch, token) => {
+  dispatch(deleteOrderStart());
+  try {
+    const res = await userRequest.delete(`/order/${id}`, {
+      headers: {
+        "Content-Type": "application/json",
+        token: `Bearer ${token}`,
+      },
+    });
+    dispatch(deleteOrderSuccess(id));
+  } catch (err) {
+    dispatch(deleteOrderFailure());
+  }
+};
+
+export const updateOrder = async (id, Order, dispatch, token) => {
+  dispatch(updateOrderStart());
+  try {
+    // update
+    const res = await userRequest.put(`/order/${id}`,Order, {
+      headers: {
+        "Content-Type": "application/json",
+        token: `Bearer ${token}`,
+      },
+    });
+    console.log(res);
+    dispatch(updateOrderSuccess({ id, Order }));
+    return 1;
+  } catch (err) {
+    dispatch(updateOrderFailure());
+    return 0;
+  }
+};
+export const addOrder = async (Order, dispatch, token) => {
+  dispatch(addOrderStart());
+  try {
+    const res = await userRequest.post(`/order/save`,Order, {
+      headers: {
+        "Content-Type": "application/json",
+        token: `Bearer ${token}`,
+      },
+    });
+    console.log(res);
+    dispatch(addOrderSuccess(res.data));
+    return 1;
+  } catch (err) {
+    dispatch(addOrderFailure());
+    return 0;
+  }
+};
+// export const countOrder = async (dispatch,token) => {
+//   dispatch(countOrderStart());
+//   try {
+//     const res = await userRequest.get(`/inventoryItem/count`);
+//     console.log(res);
+//     dispatch(countOrderSuccess(res.data));
+//     return 1;
+//   } catch (err) {
+//     dispatch(countOrderFailure());
+//     return 0;
+//   }
+// };
