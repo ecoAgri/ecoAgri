2bd346d4f5d8b4bd93ec607675b0af1a37fe7096
Pasindu-Lakmal <pasindu1lak@gmail.com>
2022-09-29 08:35:38 +0530

    page

---
+++ ecoAgriFrontEnd/src/components/ui/PageHeading.js
@@ -0,0 +1,17 @@
+import { Grid, Typography } from '@mui/material'
+import React from 'react'
+import CenteredBox from './CenteredBox'
+
+function PageHeading(props) {
+    return (
+        <Grid item xs={12} sx={{ p: 2, mx: 5, mb: 5, bgcolor: "#fff" }}>
+            <CenteredBox align="center">
+                <Typography variant="h4">
+                    {props.heading}
+                </Typography>
+            </CenteredBox>
+        </Grid>
+    )
+}
+
+export default PageHeading
