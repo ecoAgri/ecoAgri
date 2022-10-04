6f1dd16da80375fbfbbbe4099ada85ece81d849a
Pasindu-Lakmal <pasindu1lak@gmail.com>
2022-10-04 19:13:38 +0530

    firebase

---
+++ ecoAgriFrontEnd/src/Firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAbdqfn0Xc0tjlsicoPzTbnCzRmakq4Ddw",
+  authDomain: "echoagri-84301.firebaseapp.com",
+  projectId: "echoagri-84301",
+  storageBucket: "echoagri-84301.appspot.com",
+  messagingSenderId: "63733488939",
+  appId: "1:63733488939:web:64e4e4ee1816be79cab96e",
+  measurementId: "G-DCZTSQJNM2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const storage = getStorage(app);
