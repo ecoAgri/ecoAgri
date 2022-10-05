ec6a610768ef274fa4db644d00602d601558dd73
Pasindu-Lakmal <pasindu1lak@gmail.com>
2022-10-05 19:57:26 +0530

    sales

--- ecoAgriFrontEnd/src/components/articals/ArticalCard.js
+++ ecoAgriFrontEnd/src/components/articals/ArticalCard.js
@@ -16,6 +16,11 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import CardModal from './CardModal';
 import Carousel from '../ui/Carousel';
+import { Button, Grid } from '@mui/material';
+import CenteredBox from '../ui/CenteredBox';
+import { useSelector } from 'react-redux';
+import Swal from 'sweetalert2'
+// import Carousel from '../ui/Carousel/Carousel';
 
 const ExpandMore = styled((props) => {
     const { expand, ...other } = props;
@@ -30,10 +35,32 @@ const ExpandMore = styled((props) => {
 
 export default function ArticalCard() {
     const [expanded, setExpanded] = React.useState(false);
-
+    const user = useSelector((state) => state.user.currentUser);
+    const userType = user.userrole;
     const handleExpandClick = () => {
         setExpanded(!expanded);
     };
+
+    const articalConfirmHandler = (type) => {
+        Swal.fire({
+            title: `Are you want to ${type} the artical?`,
+            text: "You won't be able to revert this!",
+            icon: type === "reject" ? 'error' : 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: `Yes, ${type} it!`
+        }).then((result) => {
+            if (result.isConfirmed) {
+                //Confirmation backend call here
+                Swal.fire(
+                    `${type}!`,
+                    `Artical has been ${type}.`,
+                    'success'
+                )
+            }
+        })
+    }
 
     return (
         <Card sx={{ width: "100%" }}>
@@ -47,34 +74,21 @@ export default function ArticalCard() {
                 subheader="September 14, 2016"
             />
             <Carousel
-                steps={
-                    [{
-                        label: 'Create an ad',
-                        description: <CardMedia
-                            component="img"
-                            height="194"
-                            image="https://images.unsplash.com/photo-1499529112087-3cb3b73cec95?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
-                            alt="Paella dish"
-                        />,
-                    },
-                    {
-                        label: 'Create an ad',
-                        description: <CardMedia
-                            component="img"
-                            height="194"
-                            image="https://images.unsplash.com/photo-1501263418469-bcf7b079b4c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80"
-                            alt="Paella dish"
-                        />,
-                    },
-                    {
-                        label: 'Create an ad',
-                        description: <CardMedia
-                            component="img"
-                            height="194"
-                            image="https://images.unsplash.com/photo-1499529112087-3cb3b73cec95?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
-                            alt="Paella dish"
-                        />,
-                    },]}
+                images={
+                    [
+                        {
+                            imgPath: "https://images.unsplash.com/photo-1499529112087-3cb3b73cec95?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+                            label: "hello"
+                        },
+                        {
+                            imgPath: "https://images.unsplash.com/photo-1501263418469-bcf7b079b4c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
+                            label: "hello2"
+                        },
+                        {
+                            imgPath: "https://images.unsplash.com/photo-1499529112087-3cb3b73cec95?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+                            label: "hello3"
+                        }
+                    ]}
             />
             <CardContent>
                 <Typography variant="body1" color="text.secondary" sx={{ mb: 2 }}>
@@ -87,7 +101,19 @@ export default function ArticalCard() {
                 </Typography>
             </CardContent>
             <CardActions disableSpacing>
-                <CardModal />
+                <Grid container>
+                    <Grid item xs={4}>
+                        <CardModal onConfirm={articalConfirmHandler} />
+                    </Grid>
+                    {userType === "Moderator" &&
+                        <Grid item xs={8}>
+                            <CenteredBox align="right">
+                                <Button variant="contained" sx={{ mr: 1 }} onClick={() => { articalConfirmHandler("accept") }}>Accept</Button>
+                                <Button variant="outlined" color="error" onClick={() => { articalConfirmHandler("reject") }}>Reject</Button>
+                            </CenteredBox>
+                        </Grid>
+                    }
+                </Grid>
             </CardActions>
         </Card>
     );
