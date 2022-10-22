432d76671d7064cfef85bc5b8bc58e001ee14569
Pasindu-Lakmal <pasindu1lak@gmail.com>
2022-10-22 13:04:41 +0530

    sell

--- ecoAgriFrontEnd/src/components/pages/farmer/Sell.js
+++ ecoAgriFrontEnd/src/components/pages/farmer/Sell.js
@@ -16,40 +16,9 @@ function Sell() {
       <MainHeader value={1} />
       <Grid container sx={{ pt: "100px", px: 5 }}>
         <Grid item xs={12} className={classes.hideScrollBox}>
-          <ProductsSale />
+          <ProductsSale productType="sell" />
         </Grid>
       </Grid>
-      {/* <Grid container sx={{ pt: "100px" }} spacing={3}>
-        <PageHeading heading="Sales Details" />
-        <Grid item xs={12} sx={{ py: 5, mx: 5, bgcolor: "#fff" }}>
-          <div>
-            <Grid container>
-              <Grid item sm={12} md={8}>
-                <SalesCharts />
-              </Grid>
-              <Grid item sm={12} md={4}>
-                <Box sx={{mr: 10}}>
-                  <Grid container spacing={3}>
-                    <Grid item sm={6} md={12}>
-                      <SalePageCard bgColor="rgb(209, 233, 252)"
-                        title="Orders"
-                        total={38}
-                        icon={<VisibilityIcon></VisibilityIcon>} />
-                    </Grid>
-                    <Grid item sm={6} md={12}>
-                      <SalePageCard bgColor="rgb(209, 233, 252)"
-                        title="Cancelled Requests"
-                        total={10}
-                        icon={<VisibilityIcon></VisibilityIcon>} />
-                    </Grid>
-                  </Grid>
-                </Box>
-              </Grid>
-            </Grid>
-          </div>
-        </Grid>
-
-      </Grid> */}
     </React.Fragment>
   )
 }
