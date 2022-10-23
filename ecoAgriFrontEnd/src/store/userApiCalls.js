96804f7e61f419504984f41965f7a792bf2ddfb2
Pasindu-Lakmal <pasindu1lak@gmail.com>
2022-10-23 08:01:01 +0530

    userapi

--- ecoAgriFrontEnd/src/store/userApiCalls.js
+++ ecoAgriFrontEnd/src/store/userApiCalls.js
@@ -20,19 +20,19 @@ import { publicRequest, userRequest } from "../requestMethods";
 // import { useNavigate } from "react-router";
 
 export const register = async (User) => {
-    // dispatch(addUserStart());
-    try {
-      const res = await publicRequest.post(`/register`, User);
-      console.log(res);
-      alert("User registration Success!");
-      return 1;
-      //   dispatch(addUserSuccess(res.data));
-    } catch (err) {
-      alert("User registration Failed!");
-      return 0;
-      //   dispatch(addUserFailure());
-    }
-  };
+  // dispatch(addUserStart());
+  try {
+    const res = await publicRequest.post(`/register`, User);
+    console.log(res);
+    alert("User registration Success!");
+    return 1;
+    //   dispatch(addUserSuccess(res.data));
+  } catch (err) {
+    alert("User registration Failed!");
+    return 0;
+    //   dispatch(addUserFailure());
+  }
+};
 
 export const login = async (dispatch, user) => {
   // const userData = JSON.stringify(user);
@@ -41,42 +41,63 @@ export const login = async (dispatch, user) => {
     const res = await publicRequest.post("/login", user);
     console.log(res);
     dispatch(loginSuccess(res.data));
-    window.location.href = "http://localhost:3000/dashboard";
+    return 1;
   } catch (err) {
     dispatch(loginFailure());
+    return 0;
   }
 };
 
-export const getUsers = async (dispatch) => {
+export const getUsers = async (dispatch,token) => {
   dispatch(getUserStart());
   try {
-    const res = await userRequest.get("/users");
+    const res = await userRequest.get("/users",{
+      headers: {
+        "Content-Type": "application/json",
+        token: `Bearer ${token}`,
+      },
+    });
     dispatch(getUserSuccess(res.data));
   } catch (err) {
     dispatch(getUserFailure());
   }
 };
 
-export const deleteUser = async (id, dispatch) => {
+export const deleteUser = async (id, dispatch,token) => {
   dispatch(deleteUserStart());
   try {
-    const res = await userRequest.delete(`/user/${id}`);
+    const res = await userRequest.delete(`/user/${id}`,{
+      headers: {
+        "Content-Type": "application/json",
+        token: `Bearer ${token}`,
+      },
+    });
     dispatch(deleteUserSuccess(id));
   } catch (err) {
     dispatch(deleteUserFailure());
   }
 };
 
-export const updateUser = async (id, User, dispatch) => {
+export const updateUser = async (id, User, dispatch, token) => {
   dispatch(updateUserStart());
   try {
     // update
-    const res = await userRequest.put(`/user/${id}`);
+    const res = await userRequest.put(`/user/${id}`,{
+      headers: {
+        "Content-Type": "application/json",
+        token: `Bearer ${token}`,
+      },
+    });
     dispatch(updateUserSuccess({ id, User }));
   } catch (err) {
     dispatch(updateUserFailure());
   }
 };
+
+export const logOutUser = async (dispatch) => {
+  dispatch(logout());
+};
+
 // export const addUserWithAuth = async (User, dispatch) => {
 //   dispatch(addUserStart());
 //   try {
