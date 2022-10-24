3db940715e660b6b84d42a588cfda1de376e06b5
Dileepa-Lakmina <88375316+Dileepa-Lakmina@users.noreply.github.com>
2022-10-24 20:57:59 +0530

    Create Admin Validation

---
+++ api/Admin/adminValidate.js
@@ -0,0 +1,225 @@
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
+
+import TextContainer from "../TextContainer/TextContainer";
+import Messages from "../Messages/Messages";
+import InfoBar from "../InfoBar/InfoBar";
+import Input from "../Input/Input";
+
+import "./Chat.css";
+
+const ENDPOINT = "https://project-chat-application.herokuapp.com/";
+
+let socket;
+
+const Chat = ({ location }) => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const [users, setUsers] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+
+    setRoom(room);
+    setName(name);
+
+    socket.emit("join", { name, room }, (error) => {
+      if (error) {
+        alert(error);
+      }
+    });
+  }, [ENDPOINT, location.search]);
+
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages((messages) => [...messages, message]);
+    });
+
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
+  }, []);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage(""));
+    }
+  };
+
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
+      </div>
+      <TextContainer users={users} />
+    </div>
+  );
+};
+
+export default Chat;
+import React from "react";
+
+import onlineIcon from "../../icons/onlineIcon.png";
+import closeIcon from "../../icons/closeIcon.png";
+
+import "./InfoBar.css";
+
+const InfoBar = ({ room }) => (
+  <div className="infoBar">
+    <div className="leftInnerContainer">
+      <img className="onlineIcon" src={onlineIcon} alt="online icon" />
+      <h3>{room}</h3>
+    </div>
+    <div className="rightInnerContainer">
+      <a href="/">
+        <img src={closeIcon} alt="close icon" />
+      </a>
+    </div>
+  </div>
+);
+
+export default InfoBar;
+import React from "react";
+
+import "./Input.css";
+
+const Input = ({ setMessage, sendMessage, message }) => (
+  <form className="form">
+    <input
+      className="input"
+      type="text"
+      placeholder="Type a message..."
+      value={message}
+      onChange={({ target: { value } }) => setMessage(value)}
+      onKeyPress={(event) =>
+        event.key === "Enter" ? sendMessage(event) : null
+      }
+    />
+    <button className="sendButton" onClick={(e) => sendMessage(e)}>
+      Send
+    </button>
+  </form>
+);
+
+export default Input;
+
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import "./Join.css";
+
+export default function SignIn() {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  return (
+    <div className="joinOuterContainer">
+      <div className="joinInnerContainer">
+        <h1 className="heading">Join</h1>
+        <div>
+          <input
+            placeholder="Name"
+            className="joinInput"
+            type="text"
+            onChange={(event) => setName(event.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            placeholder="Room"
+            className="joinInput mt-20"
+            type="text"
+            onChange={(event) => setRoom(event.target.value)}
+          />
+        </div>
+        <Link
+          onClick={(e) => (!name || !room ? e.preventDefault() : null)}
+          to={`/chat?name=${name}&room=${room}`}
+        >
+          <button className={"button mt-20"} type="submit">
+            Sign In
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+import React from "react";
+
+import ScrollToBottom from "react-scroll-to-bottom";
+
+import Message from "./Message/Message";
+
+import "./Messages.css";
+
+const Messages = ({ messages, name }) => (
+  <ScrollToBottom className="messages">
+    {messages.map((message, i) => (
+      <div key={i}>
+        <Message message={message} name={name} />
+      </div>
+    ))}
+  </ScrollToBottom>
+);
+
+export default Messages;
+import React from "react";
+
+import onlineIcon from "../../icons/onlineIcon.png";
+
+import "./TextContainer.css";
+
+const TextContainer = ({ users }) => (
+  <div className="textContainer">
+    <div>
+      <h1>
+        Realtime Chat Application{" "}
+        <span role="img" aria-label="emoji">
+          💬
+        </span>
+      </h1>
+      <h2>
+        Created with React, Express, Node and Socket.IO{" "}
+        <span role="img" aria-label="emoji">
+          ❤️
+        </span>
+      </h2>
+      <h2>
+        Try it out right now!{" "}
+        <span role="img" aria-label="emoji">
+          ⬅️
+        </span>
+      </h2>
+    </div>
+    {users ? (
+      <div>
+        <h1>People currently chatting:</h1>
+        <div className="activeContainer">
+          <h2>
+            {users.map(({ name }) => (
+              <div key={name} className="activeItem">
+                {name}
+                <img alt="Online Icon" src={onlineIcon} />
+              </div>
+            ))}
+          </h2>
+        </div>
+      </div>
+    ) : null}
+  </div>
+);
+
+export default TextContainer;
