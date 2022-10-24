8843071b5b91ce8ccafabf50a984a5e67d1c31e2
Pasindu-Lakmal <pasindu1lak@gmail.com>
2022-10-24 08:59:52 +0530

    viewuser

--- ecoAgriFrontEnd/src/components/admin/ViewUserDetails.js
+++ ecoAgriFrontEnd/src/components/admin/ViewUserDetails.js
@@ -3,28 +3,29 @@ import React from 'react'
 import BankDetailField from '../farmer/sell/BankDetailField'
 
 function ViewUserDetails(props) {
+    console.log(props.data);
     return (
         <Grid container sx={{pr: 3}} spacing={3}>
             <Grid item xs={12}>
-                <BankDetailField fieldName="User Name" userDetail="Pasindu Lakmal" />
+                <BankDetailField fieldName="User Name" userDetail={props.data.col1} />
             </Grid>
             <Grid item xs={12}>
-                <BankDetailField fieldName="User Type" userDetail="Farmer" />
+                <BankDetailField fieldName="User Type" userDetail={props.data.col2} />
             </Grid>
             <Grid item xs={12}>
-                <BankDetailField fieldName="Phone Number" userDetail="1234567890" />
+                <BankDetailField fieldName="Phone Number" userDetail={props.data.phoneNumber} />
             </Grid>
             <Grid item xs={12}>
-                <BankDetailField fieldName="Email" userDetail="pasindu.lakmal@gmail.com" />
+                <BankDetailField fieldName="Email" userDetail={props.data.col3} />
             </Grid>
             <Grid item xs={12}>
-                <BankDetailField fieldName="Address" userDetail="Galle" />
+                <BankDetailField fieldName="Address" userDetail={props.data.address} />
             </Grid>
             <Grid item xs={12}>
-                <BankDetailField fieldName="City" userDetail="Galle" />
+                <BankDetailField fieldName="City" userDetail={props.data.city} />
             </Grid>
             <Grid item xs={12}>
-                <BankDetailField fieldName="Town" userDetail="Walhanduwa" />
+                <BankDetailField fieldName="Town" userDetail={props.data.town} />
             </Grid>
         </Grid>
     )
