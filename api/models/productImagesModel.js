1d20ef17a6534e10757a5cc2cb6edb40a5e7b9e2
Bhanuka Rathnayake <84070190+rmsbv@users.noreply.github.com>
2022-10-25 22:03:30 +0530

    edit prductmanagemodel

--- api/models/productImagesModel.js
+++ api/models/productImagesModel.js
@@ -26,3 +26,31 @@ const ProductImage = db.define(
 })();
 
 export default ProductImage;
+import { Sequelize } from "sequelize";
+import db from "../config/database.js";
+import Products from "./productsModel.js";
+
+const { DataTypes } = Sequelize;
+
+const ProductImage = db.define(
+  "productImage",
+  {
+    productImages: {
+      type: DataTypes.STRING,
+      allowNull: true
+      //   defaultValue: DataTypes.UUIDV4
+    },
+  },
+  {
+    freezeTableName: true,
+  }
+);
+
+// ProductImage.belongsTo(Products);
+// products.belongsToMany(productImage, { through: 'ProductProductImage' });
+
+(async () => {
+  await db.sync();
+})();
+
+export default ProductImage;
