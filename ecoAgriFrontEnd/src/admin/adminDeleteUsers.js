a1238cbcc9bca61294870b5351b5423205572366
Dileepa-Lakmina <88375316+Dileepa-Lakmina@users.noreply.github.com>
2022-10-25 21:32:01 +0530

    modify delete users functions

--- ecoAgriFrontEnd/src/admin/adminDeleteUsers.js
+++ ecoAgriFrontEnd/src/admin/adminDeleteUsers.js
@@ -548,3 +548,28 @@ function UpdateUserForm(props) {
   };
   //-------------------
 }
+function UpdateUserForm(props) {
+  console.log(props.userType);
+  const [inputs, setInputs] = useState({});
+  const [file, setFile] = useState(null);
+  const setCharityFile = (value) => {
+    setFile(value);
+  };
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  //Helpers to validate Fname and Lname
+  const hasNumber = (string) => {
+    return /\d/.test(string);
+  };
+
+  const hasSpecialChars = (string) => {
+    let pattern = /[0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+    if (string.match(pattern)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  //-------------------
+}
