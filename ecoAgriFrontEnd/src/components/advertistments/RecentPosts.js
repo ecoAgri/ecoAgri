c09acc549024d71a71965724abd1df059fe27937
Pasindu-Lakmal <pasindu1lak@gmail.com>
2022-10-26 23:49:22 +0530

    recent

---
+++ ecoAgriFrontEnd/src/components/advertistments/RecentPosts.js
@@ -0,0 +1,27 @@
+import { CardMedia, Grid, Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import React from 'react'
+import RecentPost from './RecentPost'
+
+function RecentPosts() {
+    return (
+        <React.Fragment>
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <Typography variant="h6">Recent Posts</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <RecentPost />
+                </Grid>
+                <Grid item xs={12}>
+                    <RecentPost />
+                </Grid>
+                <Grid item xs={12}>
+                    <RecentPost />
+                </Grid>
+            </Grid>
+        </React.Fragment>
+    )
+}
+
+export default RecentPosts
