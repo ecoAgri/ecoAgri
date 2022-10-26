c216ba9f60e39d73965b8ebde304f22ec6c63056
Pasindu-Lakmal <pasindu1lak@gmail.com>
2022-10-26 14:10:23 +0530

    bankdetails

--- ecoAgriFrontEnd/src/components/farmer/sell/BankDetailField.js
+++ ecoAgriFrontEnd/src/components/farmer/sell/BankDetailField.js
@@ -1,16 +1,34 @@
-import { Grid, Typography } from '@mui/material'
-import React from 'react'
+import { Grid, Typography } from "@mui/material";
+import React from "react";
 
 function BankDetailField(props) {
-    return (
-        <div>
-            <Grid container>
-                <Grid item xs={6}><Typography>{props.fieldName}</Typography></Grid>
-                <Grid item xs={1}><Typography>:</Typography></Grid>
-                <Grid item xs={5}><Typography>{props.userDetail}</Typography></Grid>
+  return (
+    <div>
+      <Grid container>
+        <Grid item xs={6}>
+          <Typography>{props.fieldName}</Typography>
+        </Grid>
+        <Grid item xs={1}>
+          <Typography>:</Typography>
+        </Grid>
+        {props.unit ? (
+          props.unit == "Rs" ? (
+            <Grid item xs={5}>
+              <Typography>Rs. {props.userDetail}</Typography>
             </Grid>
-        </div>
-    )
+          ) : (
+            <Grid item xs={5}>
+              <Typography>{props.userDetail+ " "+props.unit}</Typography>
+            </Grid>
+          )
+        ) : (
+          <Grid item xs={5}>
+            <Typography>{props.userDetail}</Typography>
+          </Grid>
+        )}
+      </Grid>
+    </div>
+  );
 }
 
-export default BankDetailField
+export default BankDetailField;
