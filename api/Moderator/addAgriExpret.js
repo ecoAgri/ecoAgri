eda0a4f69302e6d0e82cd153d94281ee0a7b1f2a
Dileepa-Lakmina <88375316+Dileepa-Lakmina@users.noreply.github.com>
2022-10-26 23:52:37 +0530

    delete and modify agri expert adding funcionalities

--- api/Moderator/addAgriExpret.js
+++ api/Moderator/addAgriExpret.js
@@ -258,13 +258,3 @@ export const fetchData = async (country) => {
     return error;
   }
 };
-
-// export const fetchDailyData = async () => {
-//   try {
-//     const { data } = await axios.get(`${url}/daily`);
-
-//     return data.map(({ confirmed, deaths, reportDate: date }) => ({ confirmed: confirmed.total, deaths: deaths.total, date }));
-//   } catch (error) {
-//     return error;
-//   }
-// };
